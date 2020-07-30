c3343bb4be240362798a5e7afb44a835983a4a68
Shiminmandy <mandyshimin18@gmail.com>
2020-07-29 22:55:06 -0400

    CharDemo

---
+++ src/com/mandy/CharDemo.java
@@ -0,0 +1,24 @@
+package com.mandy;
+
+public class CharDemo {
+    public static void main(String[] args){
+        //定义一个变量存放字符'a'
+        char a='a';
+        System.out.println("a="+a);
+        char b='b';
+        System.out.println("b="+b);
+        char ch=65;
+        System.out.println("ch="+ch);
+        //a=a
+        //b=b
+        //ch=A why? 65对应ASCII码里面的A
+
+        char ch1=65535;
+        System.out.println("ch1="+ch1);
+        //如果字面值超出char类型所表示的数据范围，需要进行强制类型转换
+        char ch2=(char)65536;
+        System.out.println("ch1="+ch2);
+        //ch1=￿
+        //ch1=
+    }
+}
