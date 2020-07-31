3a7a158e65543972b8deef278d0849ad2102a3c4
Shiminmandy <mandyshimin18@gmail.com>
2020-07-31 18:33:05 -0400

    MathDemo1

---
+++ src/com/operator/MathDemo1.java
@@ -0,0 +1,24 @@
+package com.operator;
+
+public class MathDemo1 {
+    public static void main(String[] args){
+        //自加减运算 x++
+        int x=4;
+        int y=(x++)+5;
+        System.out.println("x="+x+",y="+y);//x=5,y=9  ++在后面则先进行运算，后进行自加减
+        //++x的形式
+        x=4;
+        y=(++x)+5;
+        System.out.println("x="+x+",y="+y);//x=5,y=10 ++在前面则先进行自加减运算
+        //x--的形式
+        x=4;
+        y=(x--)+5;
+        System.out.println("x="+x+",y="+y);//x=3,y=9 先运算后自减
+        //--x的形式
+        x=4;
+        y=(--x)+5;
+        System.out.println("x="+x+",y="+y);//x=3,y=8 先自减后运算
+
+        
+    }
+}
