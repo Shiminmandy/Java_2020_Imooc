f969b8b536d8d86242aa7804fca03defa6ab0b63
Shiminmandy <mandyshimin18@gmail.com>
2020-07-30 23:56:40 -0400

    FinalDemo

---
+++ src/com/mandy/FinalDemo.java
@@ -0,0 +1,10 @@
+package com.mandy;
+
+public class FinalDemo {
+    public static void main(String[] args){
+        int m=5;
+        final int N=6; // 加上final后常量N不可改变，常量名一般大写
+        final double PI=3.14;
+        final double MIN_VALUE=0;
+    }
+}
