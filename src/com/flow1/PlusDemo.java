4c151239b59bcefd4752133838b12cf0c4c97df5
Shiminmandy <mandyshimin18@gmail.com>
2020-08-04 21:22:42 -0400

    PlusDemo

---
+++ src/com/flow1/PlusDemo.java
@@ -0,0 +1,16 @@
+package com.flow1;
+
+public class PlusDemo {
+    public static void main(String[] args){
+        //求1到5的累加和
+        //1+2+3+4+5
+        int n=1;
+        int sum=0;//sum是存放和的变量
+        while(n<=5){
+            sum=sum+n;
+            n++;
+            System.out.println("1到"+(n-1)+"的累加和为 :"+sum);
+        }
+
+    }
+}
