e3e955dd770117c14711aaa81477ddd16e1ee9d4
Shiminmandy <mandyshimin18@gmail.com>
2020-08-13 23:48:40 -0400

    Rectangle

---
+++ src/com/method/Rectangle.java
@@ -0,0 +1,15 @@
+package com.method;
+
+public class Rectangle {
+    //求长方形面积的方法
+    public int area(){
+        int length=10;
+        int width=5;
+        int area=length*width;
+        return area;//返回语句
+    }
+    public static void main(String[] args){
+        Rectangle rc=new Rectangle();
+        System.out.println("长方形的面积为："+rc.area());
+    }
+}
